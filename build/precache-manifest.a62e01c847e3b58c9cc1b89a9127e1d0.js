cb4e77c92eef3d84938c948672d18ddfa7d0d6ce
Haseeb <haseebkhan2561@gmail.com>
2020-07-04 10:08:20 -0400

    Final Version

---
+++ build/precache-manifest.a62e01c847e3b58c9cc1b89a9127e1d0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e411f4779cdb9c73c546bfba0b2d9e13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029794878c8f339af1f8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "203b35addeaca78bb4ae",
+    "url": "/static/css/main.8744c3ec.chunk.css"
+  },
+  {
+    "revision": "029794878c8f339af1f8",
+    "url": "/static/js/2.83019ee8.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.83019ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203b35addeaca78bb4ae",
+    "url": "/static/js/main.a172cd3c.chunk.js"
+  },
+  {
+    "revision": "be346c5dd7620a7163af",
+    "url": "/static/js/runtime-main.a2f624dd.js"
+  },
+  {
+    "revision": "b9e728b6adbc7bed67a700c65ebdfd88",
+    "url": "/static/media/1593267551254-screencapture-reactecommercesample-herokuapp-com-shopping-cart-1591959645480.b9e728b6.png"
+  },
+  {
+    "revision": "bd6a9a7a8fc1b9ac427cd8fc730a48a2",
+    "url": "/static/media/1593268123209-laura-chouette-8BIWYVu0vE0-unsplash.bd6a9a7a.jpg"
+  },
+  {
+    "revision": "57c495d9248afa2f22645a57b20294ee",
+    "url": "/static/media/1593268491849-sweetgun-zLPSV-RiT8w-unsplash.57c495d9.jpg"
+  },
+  {
+    "revision": "dc386f0b36c8cfdbe2f0efbbb5c83781",
+    "url": "/static/media/Notable-Regular.dc386f0b.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "84df5d4d34900ffeb60a5116466950b3",
+    "url": "/static/media/brooke-lark-W1B2LpQOBxA-unsplash.84df5d4d.jpg"
+  },
+  {
+    "revision": "8d386b64a5598b99ae0ad8bc57dcbfec",
+    "url": "/static/media/grocery_store_main_image.8d386b64.jpg"
+  },
+  {
+    "revision": "032133483f160e0cf059edc2ba138b77",
+    "url": "/static/media/kristina-tamasauskaite-Xd5DL2tASeE-unsplash.03213348.jpg"
+  },
+  {
+    "revision": "bd6a9a7a8fc1b9ac427cd8fc730a48a2",
+    "url": "/static/media/laura-chouette-8BIWYVu0vE0-unsplash.bd6a9a7a.jpg"
+  },
+  {
+    "revision": "6d27942d5bf46d8d1043e9be567a5852",
+    "url": "/static/media/pedram-normohamadian-53Z9fiDJhx8-unsplash.6d27942d.jpg"
+  },
+  {
+    "revision": "abf38ad8b868f3d816c8c50506309658",
+    "url": "/static/media/sandra-seitamaa-ew-k5JTl1xw-unsplash.abf38ad8.jpg"
+  },
+  {
+    "revision": "b9e728b6adbc7bed67a700c65ebdfd88",
+    "url": "/static/media/screencapture-reactecommercesample-herokuapp-com-shopping-cart-1591959645480.b9e728b6.png-1593267350150"
+  },
+  {
+    "revision": "57c495d9248afa2f22645a57b20294ee",
+    "url": "/static/media/sweetgun-zLPSV-RiT8w-unsplash.57c495d9.jpg"
+  },
+  {
+    "revision": "17a1d9841af4fa45124259669fbfb5db",
+    "url": "/static/media/weston-mackinnon-w-83h_clkvI-unsplash.17a1d984.jpg"
+  }
+]);
